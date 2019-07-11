8bffb5531d905d483f64d4516ed69affab2cd1f0
FireMasterK <kavin@rape.lol>
2019-07-11 16:58:09 +0530

    add missing permission message

--- src/main/java/me/kavin/fastticket/command/CommandExecutor.java
+++ src/main/java/me/kavin/fastticket/command/CommandExecutor.java
@@ -3,6 +3,7 @@ package me.kavin.fastticket.command;
 import me.kavin.fastticket.utils.EmbedUtils;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.exceptions.InsufficientPermissionException;
 
 public class CommandExecutor implements Runnable {
 
@@ -18,6 +19,10 @@ public class CommandExecutor implements Runnable {
 	public void run() {
 		try {
 			cmd.onCommand(event.getMessage().getContentRaw(), event);
+		} catch (InsufficientPermissionException e) {
+			EmbedBuilder meb = EmbedUtils.getEmptyEmbedBuilder(event.getGuild().getIdLong());
+			meb.setDescription("Cannot execute command!\nMissing permission: " + e.getPermission().getName());
+			event.getChannel().sendMessage(meb.build()).queue();
 		} catch (Throwable t) {
 			t.printStackTrace();
 			EmbedBuilder meb = EmbedUtils.getEmptyEmbedBuilder(event.getGuild().getIdLong());
