333e7d38bcfbb6f0cd3c6ebe7145630a2d86c2dd
FireMasterK <kavin@rape.lol>
2019-08-17 18:06:53 +0530

    add credits to every message

--- src/main/java/me/kavin/fastticket/utils/EmbedUtils.java
+++ src/main/java/me/kavin/fastticket/utils/EmbedUtils.java
@@ -8,6 +8,8 @@ public class EmbedUtils {
 		EmbedBuilder meb = new EmbedBuilder();
 		meb.setTitle("Fast Ticket: ");
 		meb.setColor(SettingsHelper.getInstance().getEmbedColor(guildId));
+		meb.setFooter("Bot made by Kavin#1488",
+				"https://cdn.discordapp.com/avatars/425644988487958528/fde16e409671c8ea8fd8054586c805a9.png?size=128");
 		return meb;
 	}
 }
