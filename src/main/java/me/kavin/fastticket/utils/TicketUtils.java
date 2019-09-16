5821788037404ea08f35006ce827c2f16211873b
FireMasterK <kavinsundar@gmail.com>
2019-09-16 11:56:35 +0000

    Fix issue where logs aren't sent to user

--- src/main/java/me/kavin/fastticket/utils/TicketUtils.java
+++ src/main/java/me/kavin/fastticket/utils/TicketUtils.java
@@ -25,119 +25,119 @@ import net.dv8tion.jda.api.requests.restaction.ChannelAction;
 
 public class TicketUtils {
 
-	public static void postLogs(String reason, long guildId, User closed, User opened, TextChannel tc) {
-		TextChannel lc = SettingsHelper.getInstance().getLogsChannel(guildId);
-		if (lc != null) {
+    public static void postLogs(String reason, long guildId, User closed, User opened, TextChannel tc) {
+        TextChannel lc = SettingsHelper.getInstance().getLogsChannel(guildId);
 
-			EmbedBuilder logsBuilder = EmbedUtils.getEmptyEmbedBuilder(guildId);
-			logsBuilder.setTitle("Fast Ticket: Ticket Closed");
-			StringBuilder sb = new StringBuilder();
+        EmbedBuilder logsBuilder = EmbedUtils.getEmptyEmbedBuilder(guildId);
+        logsBuilder.setTitle("Fast Ticket: Ticket Closed");
+        StringBuilder sb = new StringBuilder();
 
-			sb.append("# Fast Ticket");
-			sb.append("\n");
-			sb.append("Name | Message");
-			sb.append("\n");
-			sb.append("--- | ---");
-			sb.append("\n");
+        sb.append("# Fast Ticket");
+        sb.append("\n");
+        sb.append("Name | Message");
+        sb.append("\n");
+        sb.append("--- | ---");
+        sb.append("\n");
 
-			List<Message> msgs = new ArrayList<>();
+        List<Message> msgs = new ArrayList<>();
 
-			for (Message msg : tc.getIterableHistory())
-				msgs.add(msg);
+        for (Message msg : tc.getIterableHistory())
+            msgs.add(msg);
 
-			Collections.reverse(msgs);
+        Collections.reverse(msgs);
 
-			for (Message msg : msgs) {
-				if (msg.getAuthor().isBot())
-					continue;
-				String content = msg.getContentDisplay().replace("\n", " ").replace("\r", " ");
-				List<Attachment> attachments = msg.getAttachments();
-				if (attachments.size() > 0)
-					for (Attachment attachment : attachments)
-						content += " [attachment - " + attachment.getFileName() + "](" + attachment.getUrl() + ")";
-				sb.append(msg.getAuthor().getName() + " | " + content);
-				sb.append("\n");
-			}
+        for (Message msg : msgs) {
+            if (msg.getAuthor().isBot())
+                continue;
+            String content = msg.getContentDisplay().replace("\n", " ").replace("\r", " ");
+            List<Attachment> attachments = msg.getAttachments();
+            if (attachments.size() > 0)
+                for (Attachment attachment : attachments)
+                    content += " [attachment - " + attachment.getFileName() + "](" + attachment.getUrl() + ")";
+            sb.append(msg.getAuthor().getName() + " | " + content);
+            sb.append("\n");
+        }
 
-			if (opened != null) {
-				sb.append("\n\n");
+        if (opened != null) {
+            sb.append("\n\n");
 
-				sb.append("Opened by " + opened.getName());
-			}
+            sb.append("Opened by " + opened.getName());
+        }
 
-			if (closed != null) {
-				sb.append("\n\n");
+        if (closed != null) {
+            sb.append("\n\n");
 
-				sb.append("Closed by " + closed.getName());
-			}
+            sb.append("Closed by " + closed.getName());
+        }
 
-			JSONObject resp = new JSONObject(Unirest.post("https://api.github.com/gists")
-					.header("Authorization", Constants.GITHUB_AUTH_HEADER)
-					.body(new JSONObject().put("description", "Ticket Log").put("public", false).put("files",
-							new JSONObject().put("log.md", new JSONObject().put("content", String.valueOf(sb)))))
-					.asString().getBody());
+        JSONObject resp = new JSONObject(
+                Unirest.post("https://api.github.com/gists").header("Authorization", Constants.GITHUB_AUTH_HEADER)
+                        .body(new JSONObject().put("description", "Ticket Log").put("public", false).put("files",
+                                new JSONObject().put("log.md", new JSONObject().put("content", String.valueOf(sb)))))
+                        .asString().getBody());
 
-			String logsUrl = resp.getString("html_url") + "#file-log-md";
+        String logsUrl = resp.getString("html_url") + "#file-log-md";
 
-			logsBuilder.setDescription("Ticket closed by " + closed.getAsTag() + "\n" + "Ticket opened by "
-					+ (opened != null ? opened.getAsTag() : "User left") + "\n\n" + "**Reason:**" + "\n"
-					+ (reason != null ? reason : "No reason provied") + "\n\n" + "Logs: [click here](" + logsUrl + ")");
+        logsBuilder.setDescription("Ticket closed by " + closed.getAsTag() + "\n" + "Ticket opened by "
+                + (opened != null ? opened.getAsTag() : "User left") + "\n\n" + "**Reason:**" + "\n"
+                + (reason != null ? reason : "No reason provied") + "\n\n" + "Logs: [click here](" + logsUrl + ")");
 
-			try {
-				if (opened != null)
-					opened.openPrivateChannel().submit().get().sendMessage(logsBuilder.build()).queue();
-			} catch (Exception e) {
-				// ignored
-			}
+        if (lc != null)
+            lc.sendMessage(logsBuilder.build()).queue();
 
-			lc.sendMessage(logsBuilder.build()).queue();
-		}
-	}
+        try {
+            if (opened != null)
+                opened.openPrivateChannel().submit().get().sendMessage(logsBuilder.build()).queue();
+        } catch (Exception e) {
+            // ignored
+        }
 
-	public static TextChannel createTicket(Guild guild, Member member, String reason)
-			throws InterruptedException, ExecutionException {
+    }
 
-		Role role = SettingsHelper.getInstance().getTicketRole(guild.getIdLong());
+    public static TextChannel createTicket(Guild guild, Member member, String reason)
+            throws InterruptedException, ExecutionException {
 
-		ChannelAction<TextChannel> action = guild
-				.createTextChannel("ticket-" + SettingsHelper.getInstance().getTicketNumber(guild.getIdLong()))
-				.setTopic("ticket" + "|" + member.getId())
-				.addPermissionOverride(member, Arrays.asList(Permission.MESSAGE_READ, Permission.MESSAGE_WRITE),
-						Collections.emptyList())
-				.addPermissionOverride(guild.getPublicRole(), Collections.emptyList(),
-						Arrays.asList(Permission.MESSAGE_READ, Permission.MESSAGE_WRITE))
-				.addPermissionOverride(guild.getMember(Main.api.getShards().get(0).getSelfUser()),
-						Arrays.asList(Permission.MESSAGE_READ, Permission.MESSAGE_WRITE), Collections.emptyList());
+        Role role = SettingsHelper.getInstance().getTicketRole(guild.getIdLong());
 
-		if (role != null) {
-			action.addPermissionOverride(role, Arrays.asList(Permission.MESSAGE_READ, Permission.MESSAGE_WRITE),
-					Collections.emptyList());
-		}
+        ChannelAction<TextChannel> action = guild
+                .createTextChannel("ticket-" + SettingsHelper.getInstance().getTicketNumber(guild.getIdLong()))
+                .setTopic("ticket" + "|" + member.getId())
+                .addPermissionOverride(member, Arrays.asList(Permission.MESSAGE_READ, Permission.MESSAGE_WRITE),
+                        Collections.emptyList())
+                .addPermissionOverride(guild.getPublicRole(), Collections.emptyList(),
+                        Arrays.asList(Permission.MESSAGE_READ, Permission.MESSAGE_WRITE))
+                .addPermissionOverride(guild.getMember(Main.api.getShards().get(0).getSelfUser()),
+                        Arrays.asList(Permission.MESSAGE_READ, Permission.MESSAGE_WRITE), Collections.emptyList());
 
-		TextChannel tc = action.submit().get();
+        if (role != null) {
+            action.addPermissionOverride(role, Arrays.asList(Permission.MESSAGE_READ, Permission.MESSAGE_WRITE),
+                    Collections.emptyList());
+        }
 
-		{
-			EmbedBuilder meb = EmbedUtils.getEmptyEmbedBuilder(guild.getIdLong());
-			meb.setDescription(SettingsHelper.getInstance().getTicketOpenMsg(guild.getIdLong())
-					.replace("%user%", member.getUser().getAsTag())
-					.replace("%reason%", reason != null ? reason : "No reason provided!"));
+        TextChannel tc = action.submit().get();
+
+        {
+            EmbedBuilder meb = EmbedUtils.getEmptyEmbedBuilder(guild.getIdLong());
+            meb.setDescription(SettingsHelper.getInstance().getTicketOpenMsg(guild.getIdLong())
+                    .replace("%user%", member.getUser().getAsTag())
+                    .replace("%reason%", reason != null ? reason : "No reason provided!"));
 
             Message msg = tc.sendMessage(meb.build()).submit().get();
 
             Emote emote = Main.api.getEmoteById(Constants.CLOSE_EMOJI_ID);
 
             msg.addReaction(emote).queue();
-		}
+        }
 
-		if (role != null && SettingsHelper.getInstance().getShouldPing(guild.getIdLong())) {
+        if (role != null && SettingsHelper.getInstance().getShouldPing(guild.getIdLong())) {
 
-			role.getManager().setMentionable(true).submit().get();
+            role.getManager().setMentionable(true).submit().get();
 
-			tc.sendMessage(role.getAsMention()).submit().get();
+            tc.sendMessage(role.getAsMention()).submit().get();
 
-			role.getManager().setMentionable(false).submit().get();
-		}
+            role.getManager().setMentionable(false).submit().get();
+        }
 
-		return tc;
-	}
+        return tc;
+    }
 }
